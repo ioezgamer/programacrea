88c880e699650bce64bf5cdc3c4bdc23ee4c8004
ioezgamer <jo18mar@gmai.com>
2025-04-23 14:51:23 -0600

    Corrijo migraciones

--- database/migrations/2025_04_23_200019_create_asistencias_table.php
+++ database/migrations/2025_04_23_200019_create_asistencias_table.php
@@ -9,11 +9,10 @@ class CreateAsistenciasTable extends Migration
     public function up()
     {
         Schema::create('asistencias', function (Blueprint $table) {
-            $table->id('asistencia_id');
+            $table->id();
             $table->foreignId('participant_id')
-                ->constrained('participants') // Laravel asume que es id()
+                ->constrained(table: 'participants', column: 'participant_id')
                 ->onDelete('cascade');
-
             $table->date('fecha_asistencia');
             $table->enum('estado', ['Presente', 'Ausente', 'Justificado'])->default('Ausente')->after('fecha_asistencia');
             $table->timestamps();

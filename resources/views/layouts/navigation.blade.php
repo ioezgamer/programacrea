eafcd4a228e3a0fa3a792022dd1b6779fa7e77a6
ioezgamer <jo18mar@gmai.com>
2025-04-23 16:01:41 -0600

    Agregar asistencia

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -22,7 +22,7 @@
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('asistencia.attendance')" :active="request()->routeIs('asistencia')">
+                    <x-nav-link :href="route('asistencia.create')" :active="request()->routeIs('asistencia')">
                         {{ __('Asistencia') }}
                     </x-nav-link>
                 </div>
